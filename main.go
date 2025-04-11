35a47321749ba9c0e0e8c0bc2305ff23054f6678
limei <lmliang11@foxmail.com>
2025-04-11 15:36:56 +0800

    *

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("gorm version:")
+	s := make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap", cap(s))
+
+	var t []string
+	fmt.Println("emp:", t, "len:", len(t), "cap", cap(t))
+}
+
+func split(sum int) (int, int) {
+	var x = sum * 4 / 9
+	y := sum - x
+	return x, y
 }
